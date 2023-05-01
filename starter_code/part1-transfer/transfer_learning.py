8969d0ea336f5a489e914e633e50dd32372ff02c
Osama Hosameldeen <42114005+mohandesosama@users.noreply.github.com>
2023-05-01 23:17:30 +0400

    Update transfer_learning.py

--- starter_code/part1-transfer/transfer_learning.py
+++ starter_code/part1-transfer/transfer_learning.py
@@ -1,7 +1,7 @@
 # Starter code for Part 1 of the Small Data Solutions Project
 # 
 
-#Set up image data for train and test
+# Set up image data for train and test
 
 import torch
 import torch.nn as nn
@@ -17,29 +17,55 @@ from torchvision import models
 mean = [0.485, 0.456, 0.406]
 std = [0.229, 0.224, 0.225]
 
-#Set up Transforms (train, val, and test)
+# Set up Transforms (train, val, and test)
 
-#<<<YOUR CODE HERE>>>
+data_transforms = {
+    'train': transforms.Compose([
+        transforms.RandomResizedCrop(224),
+        transforms.RandomHorizontalFlip(),
+        transforms.ToTensor(),
+        transforms.Normalize(mean, std)
+    ]),
+    'val': transforms.Compose([
+        transforms.Resize(256),
+        transforms.CenterCrop(224),
+        transforms.ToTensor(),
+        transforms.Normalize(mean, std)
+    ]),
+    'test': transforms.Compose([
+        transforms.Resize(256),
+        transforms.CenterCrop(224),
+        transforms.ToTensor(),
+        transforms.Normalize(mean, std)
+    ])
+}
 
+# Set up DataLoaders (train, val, and test)
 
-
-
-#Set up DataLoaders (train, val, and test)
+data_dir = 'data/'
 batch_size = 10
 num_workers = 4
 
-#<<<YOUR CODE HERE>>>
+image_datasets = {x: datasets.ImageFolder(data_dir + x, data_transforms[x])
+                  for x in ['train', 'val', 'test']}
+class_names = image_datasets['train'].classes
 
-#hint, create a variable that contains the class_names. You can get them from the ImageFolder
-
-
+dataloaders = {x: torch.utils.data.DataLoader(image_datasets[x], batch_size=batch_size,
+                                             shuffle=True, num_workers=num_workers)
+              for x in ['train', 'val', 'test']}
 
 # Using the VGG16 model for transfer learning 
 # 1. Get trained model weights
 # 2. Freeze layers so they won't all be trained again with our data
-# 3. Replace top layer classifier with a classifer for our 3 categories
+# 3. Replace top layer classifier with a classifier for our 3 categories
 
-#<<<YOUR CODE HERE>>>
+model = models.vgg16(pretrained=True)
+
+for param in model.parameters():
+    param.requires_grad = False
+
+num_ftrs = model.classifier[6].in_features
+model.classifier[6] = nn.Linear(num_ftrs, 3)
 
 # Train model with these hyperparameters
 # 1. num_epochs 
@@ -47,14 +73,16 @@ num_workers = 4
 # 3. optimizer 
 # 4. train_lr_scheduler 
 
-#<<<YOUR CODE HERE>>>
-
+num_epochs = 10
+criterion = nn.CrossEntropyLoss()
+optimizer = optim.SGD(model.parameters(), lr=0.001, momentum=0.9)
+train_lr_scheduler = lr_scheduler.StepLR(optimizer, step_size=7, gamma=0.1)
 
 # When you have all the parameters in place, uncomment these to use the functions imported above
-#def main():
-#   trained_model = train_model(model, criterion, optimizer, train_lr_scheduler, train_loader, val_loader, num_epochs=num_epochs)
-#   test_model(test_loader, trained_model, class_names)
+# def main():
+#     trained_model = train_model(model, criterion, optimizer, train_lr_scheduler, dataloaders['train'], dataloaders['val'], num_epochs=num_epochs)
+#     test_model(dataloaders['test'], trained_model, class_names)
 
-#if __name__ == '__main__':
-#    main()
-#    print("done")
+# if __name__ == '__main__':
+#     main()
+#     print("done")
